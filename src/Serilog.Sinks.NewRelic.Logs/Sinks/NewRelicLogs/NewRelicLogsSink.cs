a56c3b336484ce4e4af36a8fd05a3e4e3618d05a
Roberto R. Almeida <roberto.almeida@squadra.com.br>
2021-09-09 16:28:19 -0300

    :heavy_plus_sign: Add polly retry policy and refactoring to use HttpClient

--- src/Serilog.Sinks.NewRelic.Logs/Sinks/NewRelicLogs/NewRelicLogsSink.cs
+++ src/Serilog.Sinks.NewRelic.Logs/Sinks/NewRelicLogs/NewRelicLogsSink.cs
@@ -1,4 +1,6 @@
 using Newtonsoft.Json;
+using Polly;
+using Polly.Extensions.Http;
 using Serilog.Debugging;
 using Serilog.Events;
 using Serilog.Sinks.PeriodicBatching;
@@ -8,6 +10,8 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +19,13 @@ namespace Serilog.Sinks.NewRelic.Logs
 {
     internal class NewRelicLogsSink : PeriodicBatchingSink
     {
+        private static readonly HttpClient _client = new HttpClient
+        {
+            Timeout = TimeSpan.FromSeconds(5)
+        };
+
+        private readonly IAsyncPolicy<HttpResponseMessage> _retryPolicy;
+
         public const int DefaultBatchSizeLimit = 1000;
 
         public static readonly TimeSpan DefaultPeriod = TimeSpan.FromSeconds(2);
@@ -44,6 +55,27 @@ namespace Serilog.Sinks.NewRelic.Logs
             this.LicenseKey = licenseKey;
             this.InsertKey = insertKey;
             this.FormatProvider = formatProvider;
+
+            ConfigureHttpClient();
+
+            _retryPolicy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => ((int)msg.StatusCode) >= 300)
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
+        private void ConfigureHttpClient()
+        {
+            if (!string.IsNullOrWhiteSpace(LicenseKey))
+            {
+                SelfLog.WriteLine("X-License-Key is configured by NEW_RELIC_LICENSE_KEY environment variable.");
+                _client.DefaultRequestHeaders.TryAddWithoutValidation("X-License-Key", LicenseKey);
+            }
+            else if (!string.IsNullOrWhiteSpace(InsertKey))
+            {
+                SelfLog.WriteLine("X-Insert-Key is configured by NEW_RELIC_INSERT_KEY environment variable.");
+                _client.DefaultRequestHeaders.TryAddWithoutValidation("X-Insert-Key", InsertKey);
+            }
         }
 
         protected override async Task EmitBatchAsync(IEnumerable<LogEvent> eventsEnumerable)
@@ -70,72 +102,47 @@ namespace Serilog.Sinks.NewRelic.Logs
             await this.SendToNewRelicLogsAsync(body).ConfigureAwait(false);
         }
 
-        private Task SendToNewRelicLogsAsync(string body)
+        private async Task SendToNewRelicLogsAsync(string body)
         {
-            return Task.Run(() =>
+            try
             {
-                try
-                {
-                    // add something like polly to try 3 times
-                    this.SendToNewRelicLogs(body);
-                }
-                catch (Exception ex)
-                {
-                    SelfLog.WriteLine("Event batch could not be sent to NewRelic Logs and was dropped: {0} {1}", ex.Message, ex.StackTrace);
-                }
-            });
+                await this.SendToNewRelicLogs(body);
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine("Event batch could not be sent to NewRelic Logs and was dropped: {0} {1}", ex.Message, ex.StackTrace);
+            }
         }
 
-        private void SendToNewRelicLogs(string body)
+        private async Task SendToNewRelicLogs(string body)
         {
             ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
 
-            if (!(WebRequest.Create(this.EndpointUrl) is HttpWebRequest request))
-            {
+            if (string.IsNullOrWhiteSpace(LicenseKey) && string.IsNullOrWhiteSpace(InsertKey))
                 return;
-            }
-
-            if (!string.IsNullOrWhiteSpace(this.LicenseKey))
-            {
-                request.Headers.Add("X-License-Key", this.LicenseKey);
-            }
-            else
-            {
-                request.Headers.Add("X-Insert-Key", this.InsertKey);
-            }
-
-            request.Headers.Add("Content-Encoding", "gzip");
-            request.Timeout = 40000; //It's basically fire-and-forget
-            request.Credentials = CredentialCache.DefaultCredentials;
-            request.ContentType = "application/gzip";
-            request.Accept = "*/*";
-            request.Method = "POST";
-            request.KeepAlive = false;
 
             var byteStream = Encoding.UTF8.GetBytes(body);
 
             try
             {
-                using (var zippedRequestStream = new GZipStream(request.GetRequestStream(), CompressionMode.Compress))
+                using (var memoryStream = new MemoryStream())
                 {
-                    zippedRequestStream.Write(byteStream, 0, byteStream.Length);
-                    zippedRequestStream.Flush();
-                    zippedRequestStream.Close();
-                }
-            }
-            catch (WebException ex)
-            {
-                SelfLog.WriteLine("Failed to create WebRequest to NewRelic Logs: {0} {1}", ex.Message, ex.StackTrace);
-                return;
-            }
+                    using (var gzip = new GZipStream(memoryStream, CompressionMode.Compress, true))
+                        gzip.Write(byteStream, 0, byteStream.Length);
 
-            try
-            {
-                using (var response = request.GetResponse() as HttpWebResponse)
-                {
-                    if (response == null || response.StatusCode != HttpStatusCode.Accepted)
+                    memoryStream.Position = 0;
+
+                    using (StreamContent content = new StreamContent(memoryStream))
                     {
-                        SelfLog.WriteLine("Self-log: Response from NewRelic Logs is missing or negative: {0}", response?.StatusCode);
+                        content.Headers.ContentType = new MediaTypeHeaderValue("application/gzip");
+                        content.Headers.ContentEncoding.Add("gzip");
+
+                        var response = await _retryPolicy.ExecuteAsync(async () => await _client.PostAsync(this.EndpointUrl, content));
+
+                        if (response == null || response.StatusCode != HttpStatusCode.Accepted)
+                        {
+                            SelfLog.WriteLine("Self-log: Response from NewRelic Logs is missing or negative: {0}", response?.StatusCode);
+                        }
                     }
                 }
             }
